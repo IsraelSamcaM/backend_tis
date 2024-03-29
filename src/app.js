5690e13b6915efba9f254ae444c3cade17764435
jozs83 <samca83@gmail.com>
2024-03-29 19:31:46 -0400

    escucha los servicios CORS

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors"
 //import morgan from "morgan";
 
 const app = express();
@@ -12,9 +13,9 @@ import periodosRoutes from "./routes/periodos.routes.js";
 import gruposRoutes from "./routes/grupos.routes.js";
 
 // Middlewares
-// app.use(morgan("dev"));
-app.use(express.json());
 
+app.use(express.json());
+app.use(cors());
 // Routes
 //app.use(ambientesRoutes);
 app.use("/api/ambientes", ambientesRoutes );
@@ -23,5 +24,12 @@ app.use("/api/gestiones", gestionesRoutes );
 app.use("/api/materias", materiasRoutes );
 app.use("/api/periodos", periodosRoutes );
 app.use("/api/grupos", gruposRoutes );
+
+app.get("/", (req, res) => {
+    res.json({ message: {
+        title: "¡Bienvenido al backend para TIS!",
+        description: "Gracias por usar nuestros servicios."
+    }});
+  });
 
 export default app;
