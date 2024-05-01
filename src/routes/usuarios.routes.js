3cfc98357c945d7f8f40f4e8a894024e8d2449d4
jozs83 <samca83@gmail.com>
2024-05-01 17:38:18 -0400

    Servicio de materias y grupos de asociados terminado

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,14 +1,21 @@
 import { Router } from "express";
-import { createUsuario, getUsuario, getUsuarios, updateUsuario, deleteUsuario,getUsuarioGrupo,getMateriasGrupos} from '../controllers/usuarios.controller.js';
+import {createUsuario, getUsuario, getUsuarios, 
+        updateUsuario, deleteUsuario, getUsuarioGrupo, 
+        getMateriasGrupos,getMateriasAsociados} from '../controllers/usuarios.controller.js';
 
 const router = Router();
 
 router.get('/', getUsuarios);
 router.get('/:id_usuario', getUsuario);
+
 router.post('/', createUsuario);
 router.put('/:id_usuario', updateUsuario);
 router.delete('/:id_usuario', deleteUsuario);
 router.get('/:id_usuario/materias-grupos', getMateriasGrupos);
+
+router.post('/materias-grupos-asociados', getMateriasAsociados);
+
+
 
 
 router.get('/:id_usuario/grupo', getUsuarioGrupo);
