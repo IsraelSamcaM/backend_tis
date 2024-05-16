18dc2a278b62063b6608e39810fd9f4a8c28b9ba
AleMonCayola <alejandrocayola19@gmail.com>
2024-05-16 18:21:31 -0400

    fechasFormateadas dd-mm-aaaa

--- src/controllers/aperturas.controller.js
+++ src/controllers/aperturas.controller.js
@@ -1,5 +1,10 @@
 import { Apertura } from '../models/Apertura.js';
 import { Op } from 'sequelize';
+import moment from 'moment';
+
+const formatDateTime = (dateString) => {
+    return moment(dateString).format('DD-MM-YYYY');
+};
 
 export const getAperturas = async (req, res) => {
     try {
@@ -19,10 +24,10 @@ export const getAperturasTabla = async (req, res) => {
             order: [['id_apertura', 'DESC']],
         });
 
-            const aperturasFormateadas = aperturas.map(apertura => {
+        const aperturasFormateadas = aperturas.map(apertura => {
             const inicio = apertura.apertura_inicio instanceof Date ? formatDateTime(apertura.apertura_inicio) : apertura.apertura_inicio;
             const fin = apertura.apertura_fin instanceof Date ? formatDateTime(apertura.apertura_fin) : apertura.apertura_fin;
-  
+
             let tipoUsuarioNombre = '';
             if (apertura.docente && apertura.auxiliar) {
                 tipoUsuarioNombre = 'DOCENTE - AUXILIAR';
@@ -37,13 +42,13 @@ export const getAperturasTabla = async (req, res) => {
             const periodo = `${formatDateTime(apertura.reserva_inicio)} ${formatDateTime(apertura.reserva_fin)}`;
             const now = new Date();
 
-             // Estado
-             let estado = '';
-             if (now > apertura.apertura_fin) {
-              estado = 'FINALIZADO';
-             } else if ( now <= apertura.apertura_fin) {
-              estado = 'VIGENTE';
-             }
+            // Estado
+            let estado = '';
+            if (now > apertura.apertura_fin) {
+                estado = 'FINALIZADO';
+            } else if ( now <= apertura.apertura_fin) {
+                estado = 'VIGENTE';
+            }
 
             return {
                 "inicio_apertura": `${inicioHoraMinutos} ${inicio}`,
@@ -61,22 +66,6 @@ export const getAperturasTabla = async (req, res) => {
     }
 };
 
-const formatDateTime = (date) => {
-    const formattedDate = new Date(date);
-    const formattedDateString = formattedDate.toISOString().split('T')[0];
-    return formattedDateString;
-};
-// export const getApertura = async (req, res) => {
-//     try {
-//         const { id_apertura } = req.params;
-//         const apertura = await Apertura.findOne({ where: { id_apertura } });
-//         if (!apertura) return res.status(404).json({ message: "La apertura no existe" });
-//         res.json(apertura);
-//     } catch (error) {
-//         return res.status(500).json({ message: error.message });
-//     }
-// };
-
 export const getApertura = async (req, res) => {
     try {
         const { id_apertura } = req.params;
@@ -86,15 +75,10 @@ export const getApertura = async (req, res) => {
             return res.status(404).json({ message: "La apertura no existe" });
         }
 
-        const inicio = new Date(apertura.apertura_inicio);
-        const fin = new Date(apertura.apertura_fin);
-        const reservaInicio = new Date(apertura.reserva_inicio);
-        const reservaFin = new Date(apertura.reserva_fin);
-        
-        const inicioFormateado = inicio.toISOString().split('T')[0];
-        const finFormateado = fin.toISOString().split('T')[0];
-        const reservaInicioFormateado = reservaInicio.toISOString().split('T')[0];
-        const reservaFinFormateado = reservaFin.toISOString().split('T')[0];
+        const inicioFormateado = formatDateTime(apertura.apertura_inicio);
+        const finFormateado = formatDateTime(apertura.apertura_fin);
+        const reservaInicioFormateado = formatDateTime(apertura.reserva_inicio);
+        const reservaFinFormateado = formatDateTime(apertura.reserva_fin);
 
         const aperturaFormateada = {
             id_apertura: apertura.id_apertura,
@@ -143,13 +127,11 @@ export const createApertura = async (req, res) => {
             auxiliar : esAuxiliar 
         });
 
-      
         res.status(201).json(nuevaApertura);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 };
-
 
 export const updateApertura = async (req, res) => {
     try {
@@ -181,7 +163,6 @@ export const deleteApertura = async (req, res) => {
         return res.status(500).json({ message: error.message });
     }
 };
-
 
 export const aperturaPorFecha = async (req, res) => {
     try {
